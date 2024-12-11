7dee02593082370f62b1801fef4c29a9add08a5c
acr.redb <acr.redb@gmail.com>
2024-12-11 23:44:34 +0000

    feat: finish blog page styles

--- src/components/postCard/postCard.jsx
+++ src/components/postCard/postCard.jsx
@@ -7,14 +7,16 @@ const PostCard = () => {
     <div className={styles.container}>
       <div className={styles.top}>
         <div className={styles.imgContainer}>
-          <Image src='' alt='' fill className={styles.img}/>
+          <Image src='https://images.pexels.com/photos/122442/berries-blueberries-raspberries-fruit-122442.jpeg' alt='' fill className={styles.img}/>
         </div>
-        <span className={styles.date}>01.01.2024</span>
+        <p className={styles.date}>01.01.2024</p>
       </div>
       <div className={styles.bottom}>
         <h1 className={styles.title}>Title</h1>
-        <p className={styles.desc}>desc</p>
-        <Link href='/blog/post'>Read More</Link>
+        <p className={styles.desc}>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Temporibus provident, ipsam error consequuntur quod earum. Ducimus alias impedit soluta repellat, quo, repellendus minus voluptatum quam libero qui recusandae deleniti. Similique.
+        </p>
+        <Link className={styles.link} href='/blog/post'>Read More</Link>
       </div>
     </div>
   )
